5b76a06dd85c66d477b69b8947bac55c160ff795
Marco Fabbian <marco.fabbian@gmail.com>
2024-12-01 11:46:36 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,38 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(left: List<Int>, right: List<Int>): Int {
+        if(left.size != right.size) throw IllegalStateException("The lists needs to have the same size")
+
+        return mutableListOf<Int>().apply {
+            for(i in 0..left.size-1) {
+                this.add(abs(left[i] - right[i]))
+            }
+        }.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(left: List<Int>, right: List<Int>): Int {
+        return mutableListOf<Int>().apply {
+            for(i in 0..left.size-1) {
+                this.add(abs(left[i] * right.filter { it == left[i]}.count()))
+            }
+        }.sum()
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    var leftList = mutableListOf<Int>()
+    var rightList = mutableListOf<Int>()
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    var reg = "(?<left>[0-9]+)([\\s]+)(?<right>[0-9]+)".toRegex()
+    readInput("Day01_test").forEach {
+        val matchResult = reg.find(it) !!
+        val leftValue = matchResult.groups["left"]?.value !!
+        val rightValue = matchResult.groups["right"]?.value !!
+        leftList.add(leftValue.toInt())
+        rightList.add(rightValue.toInt())
+    }
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    leftList.sort()
+    rightList.sort()
+    part1(leftList, rightList).println()
+    part2(leftList, rightList).println()
 }
